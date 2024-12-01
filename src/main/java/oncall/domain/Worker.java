c69df95d2537119878d63b30c15276cd83aa8bc0
cyunlee <siyunkr@gmail.com>
2024-12-01 21:56:08 +0900

    feat: Worker 도메인 생성자 기능 추가

--- src/main/java/oncall/domain/Worker.java
+++ src/main/java/oncall/domain/Worker.java
@@ -4,10 +4,21 @@ public class Worker {
     private String nickname;
     private int weekdaysNumber;
     private int weekendsNumber;
+    private boolean workOnWeekdays;
+    private boolean workOnWeekends;
 
     public Worker(String nickname, int weekdaysNumber, int weekendsNumber) {
         this.nickname = nickname;
         this.weekdaysNumber = weekdaysNumber;
         this.weekendsNumber = weekendsNumber;
+
+        if (weekdaysNumber!=0) {
+            this.workOnWeekdays = true;
+        }
+        if (weekendsNumber!=0) {
+            this.workOnWeekends = true;
+        }
     }
+
+    //만약 workOnWeekdays, workOnWeekends가 이미 true라면 더이상 생성할 수 없도록.
 }
