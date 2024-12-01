ca1cb7f8adb920a68f177500c377caf37005aad0
cyunlee <siyunkr@gmail.com>
2024-12-01 21:26:31 +0900

    feat: OutputView 기본 기능 구현

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -3,8 +3,16 @@ package oncall.view;
 public class OutputView {
     private final static String ERROR = "[ERROR] ";
 
-    public void printPrompt() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    public void printStartPrompt() {
+        System.out.println("비상 근무를 배정할 월과 시작 요일을 입력하세요> ");
+    }
+
+    public void printWeekdaysPrompt() {
+        System.out.println("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+    }
+
+    public void printWeekendsPrompt() {
+        System.out.println("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
     }
 
     public static void printError(String message) {
