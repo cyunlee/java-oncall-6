3ce8588dce8f9f42e046bee86a8f1a08cc68d43e
cyunlee <siyunkr@gmail.com>
2024-12-01 21:32:09 +0900

    feat: InputParser 기본 기능 구현

--- src/main/java/oncall/util/InputParser.java
+++ src/main/java/oncall/util/InputParser.java
@@ -1,4 +1,18 @@
 package oncall.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputParser {
+    public List<String> parseCastInfo(String input) {
+        return Arrays.asList(input.split(","));
+    }
+
+    public List<String> parsedWeekdaysWorkers(String input) {
+        return Arrays.asList(input.split(","));
+    }
+
+    public List<String> parseWeekendsWorkers(String input) {
+        return Arrays.asList(input.split(","));
+    }
 }
