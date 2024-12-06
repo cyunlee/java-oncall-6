57b6127713b0d1c64e66246e53f7b4025b835bb1
cyunlee <siyunkr@gmail.com>
2024-12-06 09:57:25 +0900

    feat: InputParser 메서드 구현

--- src/main/java/oncall/util/InputParser.java
+++ src/main/java/oncall/util/InputParser.java
@@ -1,4 +1,19 @@
 package oncall.util;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class InputParser {
+    public Map<Integer, String> parseMonthAndDate(String input) {
+        List<String> parsedInput = Arrays.asList(input.split(","));
+        Map<Integer, String> parsedMonthAndDate = new HashMap<>();
+        parsedMonthAndDate.put(Integer.parseInt(parsedInput.get(0)), parsedInput.get(1));
+        return parsedMonthAndDate;
+    }
+
+    public List<String> parseWorkers(String input) {
+        return Arrays.asList(input.split(","));
+    }
 }
