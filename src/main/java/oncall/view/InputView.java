40bd92f9061db45eeae32af52e68761f5361536f
cyunlee <siyunkr@gmail.com>
2024-12-06 09:54:03 +0900

    feat: InputView 기본 메서드 구현

--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -1,6 +1,7 @@
 package oncall.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Map;
 import oncall.exception.InputValidator;
 import oncall.util.InputParser;
 
@@ -8,6 +9,20 @@ public class InputView {
     private final InputParser inputParser = new InputParser();
     private final InputValidator inputValidator = new InputValidator();
 
+    public Map<Integer, String> readMonthAndDate() {
+        String input = Console.readLine();
+        return input;
+    }
+
+    public List<String> readWeekdayWorkers() {
+        String input = Console.readLine();
+        return input;
+    }
+
+    public List<String> readWeekendWorkers() {
+        String input = Console.readLine();
+        return input;
+    }
 
 
     public String readInput() {
